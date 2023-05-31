ec8e52ce4441f3b20d71cfad526f7a150d66f384
mad4869 <mad4869@gmail.com>
2023-05-31 12:11:09 +0700

    Added welcome screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-const container = document.getElementById('container')
+const clock = () => {
+    const date = new Date()
+    document.getElementById('clock').innerHTML = date.toLocaleTimeString([], { hour12: false })
+}
+
+setInterval(clock, 1000)
+
+// const doneItem = document.getElementById('done-item')
+// if (!doneItem.innerHTML) {
+//     doneItem.setAttribute('style', 'display:none;')
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    function updateProgressBar() {
+        const progressBar = document.querySelector('.progress')
+        const progress = Math.floor((performance.now() / 3000) * 100)
+        progressBar.style.width = progress + '%'
+    }
+    function hideWelcomeScreen() {
+        const welcome = document.getElementById('welcome')
+        welcome.style.display = 'none'
+        const homepage = document.getElementById('homepage')
+        homepage.style.display = 'block'
+    }
+    window.onload = function () {
+        updateProgressBar()
+        setTimeout(hideWelcomeScreen, 3000)
+    }
+})
