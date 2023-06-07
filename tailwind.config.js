9ebf69ba531439162f5557d46b5625dddcc2a53d
MAkbarD <mad4869@gmail.com>
2023-06-07 08:19:34 +0700

    Fixed welcome and login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./todo/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif']
+      },
+      backgroundImage: {
+        'login': "url('../todo/static/assets/bg-login.png')",
+        'register': "url('../todo/static/assets/bg-register.png')"
+      },
+      dropShadow: {
+        'right': '5px 0px 10px rgba(0, 0, 0, 0.3)'
+      }
+    },
   },
   plugins: [],
 }
