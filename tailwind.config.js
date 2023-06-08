ba53f7c6131fc86b1c083cc76b8fc0b7be05181a
MAkbarD <mad4869@gmail.com>
2023-06-08 19:00:19 +0700

    Fixed the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       },
       backgroundImage: {
         'login': "url('../todo/static/assets/bg-login.png')",
-        'register': "url('../todo/static/assets/bg-register.png')"
+        'register': "url('../todo/static/assets/bg-register.png')",
+        'navbar': "url('../todo/static/assets/bg-navbar.png')"
       },
       dropShadow: {
         'right': '5px 0px 10px rgba(0, 0, 0, 0.3)'
