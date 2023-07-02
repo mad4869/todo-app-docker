952c3cede30ca7e3fecfaded6acfe439dc0643d9
MAkbarD <mad4869@gmail.com>
2023-07-03 04:57:10 +0700

    Build responsive pages

--- src/js/landing/index.js
+++ src/js/landing/index.js
@@ -7,3 +7,11 @@ const flash = document.getElementById('flash')
 if (flash) {
     handleFlash()
 }
+
+const hamburger = document.getElementById('landing-hamburger-button')
+const menu = document.getElementById('landing-hamburger-menu')
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('opacity-0')
+    menu.classList.toggle('opacity-100')
+})
